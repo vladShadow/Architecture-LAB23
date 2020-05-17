35b31b3d701b35438cf8073259d0d1c661edc11e
vladShadow <vladivashchuk@gmail.com>
2020-05-17 13:03:51 +0300

    change int test lib

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -5,7 +5,15 @@ import (
 	"net/http"
 	"testing"
 	"time"
+
+	. "gopkg.in/check.v1"
 )
+
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
 
 const baseAddress = "http://balancer:8090"
 
@@ -13,13 +21,13 @@ var client = http.Client{
 	Timeout: 3 * time.Second,
 }
 
-func TestBalancer(t *testing.T) {
+func (s *MySuite) TestBalancer(c *C) {
 	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
 	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
 	if err != nil {
-		t.Error(err)
+		c.Error(err)
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+	c.Logf("response from [%s]", resp.Header.Get("lb-from"))
 }
 
 func BenchmarkBalancer(b *testing.B) {
