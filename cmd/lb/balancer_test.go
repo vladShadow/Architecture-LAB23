dc4fca599a55f422da5c978ab8a42749c1bd58ae
vladShadow <vladivashchuk@gmail.com>
2020-05-17 17:35:38 +0300

    try again

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -25,11 +25,11 @@ func (s *MySuite) TestBalancer(c *C) {
 	// для одного клієнта буде використовуватись один і той же сервер
 
 	// all servers alive
-	serversPool = []string{
-		"server1:8080",
-		"server2:8080",
-		"server3:8080",
-	}
+	// serversPool = []string{
+	// 	"server1:8080",
+	// 	"server2:8080",
+	// 	"server3:8080",
+	// }
 	var prevIndex int
 	for i := 0; i < len(clients); i++ {
 		for j := 0; i < 5; j++ {
